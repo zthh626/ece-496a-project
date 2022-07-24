3a453945dcd71c438fb1840f5f78b84bbb9c542b
Alex <zthh626@gmail.com>
2022-07-24 12:27:08 -0700

    feat: CarNFT contract

---
+++ contracts/CarNFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CarNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("CarNFT", "CAR") {}
+
+    function mintCar(string memory carTokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, carTokenURI);
+
+        return newItemId;
+    }
+}
