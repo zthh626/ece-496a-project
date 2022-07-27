f4fb880d66be647f0408cc853d8b43bd66701c58
Manav Shergill <55296628+shergillmanav19@users.noreply.github.com>
2022-07-27 11:07:56 -0700

    Create CreateKeccak256Hash.sol

---
+++ contracts/contracts/CreateKeccak256Hash.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+contract CreateKeccak256Hash {
+
+    mapping(address => bytes32) public hashes;
+
+    // initializes contract with the erc721 contract
+    constructor() {}
+
+    // The hash is the bid + a nonce
+    function findHashOfAmount(uint256 amount, uint256 nonce) public isAuctionStarted {
+        uint256 prehashedValue = amount + nonce;
+        string memory hashedString = Strings.toString(prehashedValue);
+        bytes32 hash = keccak256(abi.encode(hashedString));
+        hashes[msg.sender] = hash;
+    }
+}
