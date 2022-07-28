87c7e9d972f682908b752034de190b4be23c9a9a
Manav Shergill <55296628+shergillmanav19@users.noreply.github.com>
2022-07-28 10:28:37 -0700

    Adding comments

--- contracts/contracts/CarNFT.sol
+++ contracts/contracts/CarNFT.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+// A contract will mint a ERC721 (NFT)
 contract CarNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("CarNFT", "CAR") {}
-
+    
+    // Function to mint
     function mintCar(string memory carTokenURI) public returns (uint256) {
         _tokenIds.increment();
 
